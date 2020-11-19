40c111745a33e7fa622bd696ca1eb4c973823fe4
LukasSemler <semler.l04@htlwienwest.at>
2020-11-19 07:52:20 +0100

    Click_Umdrehen

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function select(el) {
-    el.classList.toggle("card_flipped");
-  }
+// function select(el) {
+//     el.classList.toggle("card_flipped");
+//   }
+
+// let elem = document.querySelectorAll(".card")
+
+// for (const i of elem) {
+//     i.addEventListener("click", select)
+
+// }
+
+let kartListe = document.querySelectorAll('.card')
+
+kartListe.forEach(Element => {
+  Element.addEventListener('click', (event) => {
+    event.target.classList.toggle("card_flipped")
+  })
+})
